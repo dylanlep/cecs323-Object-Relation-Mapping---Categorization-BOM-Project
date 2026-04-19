971dcc969a5a582059f707326a97381dc90a2541
Aayush-R7976 <aayushroy2005@gmail.com>
2026-04-18 19:41:13 -0700

    Update Vendor.py

--- Object Relational Mapping Project/Vendor.py
+++ Object Relational Mapping Project/Vendor.py
@@ -1,10 +1,16 @@
 from orm_base import Base
 from sqlalchemy import String, CheckConstraint
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 class Vendor(Base):
     __tablename__ = "vendors"
+
     name: Mapped[str] = mapped_column('name', String(80), primary_key=True)
+
+    piece_parts: Mapped[List["PiecePart"]] = relationship(
+        back_populates="vendor"
+    )
 
     __table_args__ = (
         CheckConstraint("length(name) >= 3", name="vendors_name_min_check"),
